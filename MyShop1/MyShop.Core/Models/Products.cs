f14b3af21a7071471bc8911e5202b53a5e0e87a2
Vladyslav <oliinyk-vlad@ukr.net>
2018-08-15 22:45:21 +0300

    Database created

--- MyShop1/MyShop.Core/Models/Products.cs
+++ MyShop1/MyShop.Core/Models/Products.cs
@@ -15,7 +15,7 @@ namespace MyShop.Core.Models
         [DisplayName("Product Name")]
         public string Name { get; set; }
         public string Description { get; set; }
-        [Range(0, 1000)]
+        [Range(0, 5000)]
         public decimal Price { get; set; }
         public string Category { get; set; }
         public string Image { get; set; }
